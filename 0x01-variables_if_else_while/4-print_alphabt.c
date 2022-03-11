258a47e07fa2d435a592f1c8354af4e2c676677d
ishimwebernard <isbernard2001@gmail.com>
2022-03-12 01:27:09 +0200

    mend

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,9 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'q' || ch != 'e')
-			putchar(ch);
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
