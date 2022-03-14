d7ea70cc9f262f4ecf515a700bd9697097bd27d7
ishimwebernard <isbernard2001@gmail.com>
2022-03-14 16:02:21 +0200

    mend

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: Return is 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
