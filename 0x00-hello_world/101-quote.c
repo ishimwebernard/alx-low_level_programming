33d6813fc7e63d7ea25d873189549251d0bd3b47
ishimwebernard <isbernard2001@gmail.com>
2022-03-14 20:54:26 +0200

    mend

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - entry point
-* Return: Always 0 (Failure)
+* Return: Always 1 (Failure)
 */
 int main(void)
 {
-	syscall(__NR_write,1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",6);
+	system("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
