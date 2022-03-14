d23644dfd652ed3a39850dc2f3b8544d04cac1aa
ishimwebernard <isbernard2001@gmail.com>
2022-03-14 16:14:08 +0200

    mend

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Returns 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
