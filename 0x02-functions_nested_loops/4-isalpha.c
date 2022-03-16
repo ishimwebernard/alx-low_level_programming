f9a3dd30aefbb9cce43deef2a49d282a5d436f88
ishimwebernard <isbernard2001@gmail.com>
2022-03-16 11:33:41 +0200

    mend

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+/**
+* _isalpha - checks if the character is alpanumeric
+* @c character to check for
+* Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
