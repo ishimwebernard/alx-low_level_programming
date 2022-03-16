a0c0f0b2d3959df69f60f47b681d0fa279a8ddfc
ishimwebernard <isbernard2001@gmail.com>
2022-03-16 14:23:30 +0200

    mend

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*
+* jack_bauer - every minute of the day
+*
+* Return: Print only return void
+*/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				_putchar(0 + '0');
+			else
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+
+			if (j < 10)
+				_putchar(0 + '0');
+			else
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
