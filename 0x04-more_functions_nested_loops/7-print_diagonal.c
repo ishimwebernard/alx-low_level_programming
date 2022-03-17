afb369fd5833a4d28156580c4b8e0b8e82c97653
ishimwebernard <isbernard2001@gmail.com>
2022-03-17 18:11:40 +0200

    mend

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  print_diagonal - print diagonal
+ * 
+ * @n nnumber of rows to print
+ */
+void print_diagonal(int n)
+{
+        int i, j;
+
+        if (n > 0)
+        for (i = 0; i < n; i++)
+        {
+                for (j = 0; j < i; j++)
+                        _putchar(' ');
+                _putchar(' ');
+                _putchar('\n');
+        }
+        else
+                _putchar('\n');
+}
