6bcac52cbed9ace6dd06516684556ee5df2e492a
ishimwebernard <isbernard2001@gmail.com>
2022-03-22 02:30:45 +0200

    mend

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,8 @@ void puts2(char *str)
 	int i;
 	int length = strlen(str);
 
-	_putchar(*str);
+	if (length != 0)
+		_putchar(*str);
 	for (i = 0; i < length && length != 0; i++)
 	{
 		if (i != 0 && i % 2 == 0)
